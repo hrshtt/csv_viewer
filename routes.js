894bb9b054a753a349c6b2ff845e8fd0bae029ec
Harshit <harshsha64@gmail.com>
2020-03-02 14:55:35 +0530

    fixed routing

--- routes.js
+++ routes.js
@@ -3,20 +3,44 @@ const router = express.Router();
 const csv = require("./csv_files");
 // console.log(fileNames);
 fileNames = csv.getFiles();
+
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/displayfile", (req, res) => {
-  res.render("displayfile", {
-    fileNames: fileNames
-  });
-});
+var csv_data;
+var keys;
+router
+  .route("/displayfile")
+  .get((req, res) => {
+    res.render("displayfile", {
+      fileNames: fileNames
+    });
+  })
+  .post((req, res) => {
+    csv_data = csv.getDataCSV(req.body.fileName);
+    keys = Object.keys(csv_data[0]);
+    // console.log(Object.keys(csv_data[0]));
 
-router.post("/displayfile", (req, res) => {
-  res.render("displayCSV", {
-    csv_data: csv.getDataCSV(req.body.fileName)
+    res
+      .status(301)
+      .location("/displayCSV")
+      .end();
   });
-});
+
+router
+  .route("/displayCSV")
+  .get((req, res) => {
+    res.render("displayCSV", {
+      csv_data: csv_data,
+      keys: keys
+    });
+  })
+  .post((req, res) => {
+    res.render("displayCSV", {
+      csv_data: csv_data,
+      keys: req.body.new_key
+    });
+  });
 
 module.exports = router;
